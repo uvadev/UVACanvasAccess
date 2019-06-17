bdc3d0ea2de202f1c6b74aaa99852a29cdc67257
Max <mmorris@uview.academy>
2019-06-17 13:18:47 -0500

    add sort and order params to GetUserList endpoint

--- UVACanvasAccess/Api.cs
+++ UVACanvasAccess/Api.cs
@@ -2,30 +2,72 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using UVACanvasAccess.Model;
 
 namespace UVACanvasAccess {
-    public class Api {
+    public class Api : IDisposable {
 
-        private readonly HttpClient client;
+        private readonly HttpClient _client;
         
         public Api(string token, string baseUrl) {
-            client = new HttpClient();
+            _client = new HttpClient();
             
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            client.BaseAddress = new Uri(baseUrl);
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            _client.BaseAddress = new Uri(baseUrl);
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             
         }
 
-        private Task<HttpResponseMessage> RawGetListUsers(string searchTerm, string accountId = "self") {
-            return client.GetAsync("accounts/" + accountId + "/users?search_term=" + searchTerm);
+        public void Dispose() {
+            _client?.Dispose();
         }
 
-        public async Task<List<User>> GetListUsers(string searchTerm, string accountId = "self") {
-            var response = await RawGetListUsers(searchTerm, accountId);
+        /// <summary>
+        /// Constructs a query string with the given key-value pairs.
+        /// </summary>
+        /// <param name="args">The key-value pairs to use in the query string. Null values are ignored.</param>
+        /// <returns>The query string.</returns>
+        private static string BuildQueryString(params ValueTuple<string, string>[] args) {
+           
+            var sb = new StringBuilder("?");
+            foreach (var (key, val) in args) {
+                if (val != null) {
+                    sb.Append($"{key}={val}&");
+                }
+            }
+
+            return sb.Remove(sb.Length - 1, 1).ToString();
+        }
+
+        private Task<HttpResponseMessage> RawGetListUsers(string searchTerm, 
+                                                          string accountId,
+                                                          string sort,
+                                                          string order) {
+            return _client.GetAsync("accounts/" + accountId + "/users" + BuildQueryString(
+                                                                                        ("search_term", searchTerm),
+                                                                                        ("sort", sort),
+                                                                                        ("order", order)
+                                                                                      ));
+        }
+        
+        /// <summary>
+        /// Get the list of users associated with the account, according to a search term.
+        /// Corresponds to the API endpoint <c>GET /api/v1/accounts/:account_id/users</c>. 
+        /// </summary>
+        /// <param name="searchTerm">The partial name or full ID of the users to match.</param>
+        /// <param name="accountId">The account to search. <c>self</c> by default.</param>
+        /// <param name="sort">The column to sort results by. Allowed values are <c>username, email, sis_id, last_login</c>.</param>
+        /// <param name="order">The order to sort the given column by. Allowed values are <c>asc, desc</c>.</param>
+        /// <returns>The list of users found in the search.</returns>
+        /// <exception cref="Exception">Thrown if the API returns a failing response code.</exception>
+        public async Task<List<User>> GetListUsers(string searchTerm,
+                                                   string sort = null,
+                                                   string order = null,
+                                                   string accountId = "self") {
+            var response = await RawGetListUsers(searchTerm, accountId, sort, order);
             if (!response.IsSuccessStatusCode) {
                 throw new Exception($"http failure response: {response.StatusCode} {response.ReasonPhrase}");
             }
--- UVACanvasAccess/Test.cs
+++ UVACanvasAccess/Test.cs
@@ -10,7 +10,7 @@ namespace UVACanvasAccess {
             var api = new Api(Environment.GetEnvironmentVariable("TEST_TOKEN"), 
                               "https://uview.instructure.com/api/v1/");
             
-            var natalies = await api.GetListUsers("Natalie");
+            var natalies = await api.GetListUsers("Natalie", "username", "desc");
             Console.WriteLine("List of Natalies ({0} natalies):", natalies.Count);
             foreach (var natalie in natalies) {
                 Console.WriteLine(natalie + "\n========\n");
