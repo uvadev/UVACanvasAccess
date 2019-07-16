867bb2e23ab9ac501d1387deba69565fc3aea58d
Max <mmorris@uview.academy>
2019-07-16 12:48:21 -0500

    Added Course structure.

---
+++ UVACanvasAccess/Model/Courses/CourseModel.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+using UVACanvasAccess.Model.Enrollments;
+
+namespace UVACanvasAccess.Model.Courses {
+    
+    // ReSharper disable MemberCanBePrivate.Global
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class CourseModel {
+        
+        [JsonProperty("id")]
+        public ulong Id { get; set; }
+        
+        [JsonProperty("sis_course_id")]
+        public string SisCourseId { get; set; }
+        
+        [JsonProperty("uuid")]
+        public string Uuid { get; set; }
+        
+        [JsonProperty("integration_id")]
+        public string IntegrationId { get; set; }
+        
+        [JsonProperty("sis_import_id")]
+        public ulong SisImportId { get; set; }
+        
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        [JsonProperty("course_code")]
+        public string CourseCode { get; set; }
+        
+        [JsonProperty("workflow_state")]
+        public string WorkflowState { get; set; }
+        
+        [JsonProperty("account_id")]
+        public ulong AccountId { get; set; }
+        
+        [JsonProperty("root_account_id")]
+        public ulong RootAccountId { get; set; }
+        
+        [JsonProperty("enrollment_term_id")]
+        public ulong EnrollmentTermId { get; set; }
+        
+        [JsonProperty("grading_standard_id")]
+        public ulong GradingStandardId { get; set; }
+        
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+        
+        [JsonProperty("start_at")]
+        public DateTime? StartAt { get; set; }
+        
+        [JsonProperty("end_at")]
+        public DateTime? EndAt { get; set; }
+        
+        [JsonProperty("locale")]
+        public string Locale { get; set; }
+        
+        [CanBeNull] 
+        [JsonProperty("enrollments")]
+        public IEnumerable<EnrollmentModel> Enrollments { get; set; }
+        
+        [JsonProperty("total_students")]
+        public ulong? TotalStudents { get; set; }
+        
+        [JsonProperty("calendar")]
+        public object Calendar { get; set; } // todo Calendar model
+        
+        [JsonProperty("default_view")]
+        public string DefaultView { get; set; }
+        
+        [JsonProperty("syllabus_body")]
+        public string SyllabusBody { get; set; }
+        
+        [JsonProperty("needs_grading_count")]
+        public uint? NeedsGradingCount { get; set; }
+        
+        [JsonProperty("term")]
+        [CanBeNull]
+        public TermModel Term { get; set; }
+        
+        [JsonProperty("course_progress")]
+        [CanBeNull]
+        public CourseProgressModel CourseProgress { get; set; }
+        
+        [JsonProperty("apply_assignment_group_weights")]
+        public bool? ApplyAssignmentGroupWeights { get; set; }
+        
+        [JsonProperty("permissions")]
+        public Dictionary<string, bool> Permissions { get; set; }
+        
+        [JsonProperty("is_public")]
+        public bool IsPublic { get; set; }
+        
+        [JsonProperty("is_public_to_auth_users")]
+        public bool IsPublicToAuthUsers { get; set; }
+        
+        [JsonProperty("public_syllabus")]
+        public bool PublicSyllabus { get; set; }
+        
+        [JsonProperty("public_syllabus_to_auth")]
+        public bool PublicSyllabusToAuth { get; set; }
+        
+        [JsonProperty("public_description")]
+        [CanBeNull]
+        public string PublicDescription { get; set; }
+        
+        [JsonProperty("storage_quota_mb")]
+        public ulong StorageQuotaMb { get; set; }
+        
+        [JsonProperty("storage_quota_used_mb")]
+        public ulong StorageQuotaUsedMb { get; set; }
+        
+        [JsonProperty("hide_final_grades")]
+        public bool HideFinalGrades { get; set; }
+        
+        [JsonProperty("license")]
+        public string License { get; set; }
+        
+        [JsonProperty("allow_student_assignment_edits")]
+        public bool AllowStudentAssignmentEdits { get; set; }
+        
+        [JsonProperty("allow_wiki_comments")]
+        public bool AllowWikiComments { get; set; }
+        
+        [JsonProperty("allow_student_forum_attachments")]
+        public bool AllowStudentForumAttachments { get; set; }
+        
+        [JsonProperty("open_enrollment")]
+        public bool OpenEnrollment { get; set; }
+        
+        [JsonProperty("self_enrollment")]
+        public bool SelfEnrollment { get; set; }
+        
+        [JsonProperty("restrict_enrollments_to_courses")]
+        public bool RestrictEnrollmentsToCourseDates { get; set; }
+        
+        [JsonProperty("course_format")]
+        public string CourseFormat { get; set; }
+        
+        [JsonProperty("access_restricted_by_date")]
+        public bool? AccessRestrictedByDate { get; set; }
+        
+        [JsonProperty("time_zone")]
+        public string TimeZone { get; set; }
+        
+        [JsonProperty("blueprint")]
+        public bool? Blueprint { get; set; }
+        
+        [CanBeNull]
+        [JsonProperty("blueprint_restrictions")]
+        public Dictionary<string, bool> BlueprintRestrictions { get; set; }
+
+        [CanBeNull]
+        [JsonProperty("blueprint_restrictions_by_object_type")]
+        public Dictionary<string, Dictionary<string, bool>> BlueprintRestrictionsByObjectType { get; set; }
+    }
+}
---
+++ UVACanvasAccess/Structures/Courses/Course.cs
@@ -0,0 +1,219 @@
+using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UVACanvasAccess.ApiParts;
+using UVACanvasAccess.Model.Courses;
+using UVACanvasAccess.Structures.Enrollments;
+using UVACanvasAccess.Util;
+
+namespace UVACanvasAccess.Structures.Courses {
+    
+    // ReSharper disable UnusedAutoPropertyAccessor.Global
+    // ReSharper disable AutoPropertyCanBeMadeGetOnly.Local
+    // ReSharper disable MemberCanBePrivate.Global
+    public class Course : IPrettyPrint {
+        private readonly Api _api;
+        
+        public ulong Id { get; }
+        
+        public string SisCourseId { get; }
+        
+        public string Uuid { get; }
+        
+        public string IntegrationId { get; }
+        
+        public ulong SisImportId { get; }
+        
+        public string Name { get; }
+        
+        public string CourseCode { get; }
+        
+        public string WorkflowState { get; }
+        
+        public ulong AccountId { get; }
+        
+        public ulong RootAccountId { get; }
+        
+        public ulong EnrollmentTermId { get; }
+        
+        public ulong GradingStandardId { get; }
+        
+        public DateTime CreatedAt { get; }
+        
+        public DateTime? StartAt { get; }
+        
+        public DateTime? EndAt { get; }
+        
+        public string Locale { get; }
+        
+        public IEnumerable<Enrollment> Enrollments { get; }
+        
+        public ulong? TotalStudents { get; }
+        
+        public object Calendar { get; } // todo Calendar structure
+        
+        public string DefaultView { get; }
+        
+        public string SyllabusBody { get; }
+        
+        public uint? NeedsGradingCount { get; }
+        
+        [CanBeNull]
+        public Term Term { get; }
+        
+        [CanBeNull]
+        public CourseProgress CourseProgress { get; }
+        
+        public bool? ApplyAssignmentGroupWeights { get; }
+        
+        public Dictionary<string, bool> Permissions { get; }
+        
+        public bool IsPublic { get; }
+        
+        public bool IsPublicToAuthUsers { get; }
+        
+        public bool PublicSyllabus { get; }
+        
+        public bool PublicSyllabusToAuth { get; }
+        
+        [CanBeNull]
+        public string PublicDescription { get; }
+        
+        public ulong StorageQuotaMb { get; }
+        
+        public ulong StorageQuotaUsedMb { get; }
+        
+        public bool HideFinalGrades { get; }
+        
+        public string License { get; }
+        
+        public bool AllowStudentAssignmentEdits { get; }
+        
+        public bool AllowWikiComments { get; }
+        
+        public bool AllowStudentForumAttachments { get; }
+        
+        public bool OpenEnrollment { get; }
+        
+        public bool SelfEnrollment { get; }
+        
+        public bool RestrictEnrollmentsToCourseDates { get; }
+        
+        public string CourseFormat { get; }
+        
+        public bool? AccessRestrictedByDate { get; }
+        
+        public string TimeZone { get; }
+        
+        public bool? Blueprint { get; }
+        
+        [CanBeNull]
+        public Dictionary<string, bool> BlueprintRestrictions { get; }
+
+        [CanBeNull]
+        public Dictionary<string, Dictionary<string, bool>> BlueprintRestrictionsByObjectType { get; }
+
+        public Course(Api api, CourseModel model) {
+            _api = api;
+            Id = model.Id;
+            SisCourseId = model.SisCourseId;
+            Uuid = model.Uuid;
+            IntegrationId = model.IntegrationId;
+            SisImportId = model.SisImportId;
+            Name = model.Name;
+            CourseCode = model.CourseCode;
+            WorkflowState = model.WorkflowState;
+            AccountId = model.AccountId;
+            RootAccountId = model.RootAccountId;
+            EnrollmentTermId = model.EnrollmentTermId;
+            GradingStandardId = model.GradingStandardId;
+            CreatedAt = model.CreatedAt;
+            StartAt = model.StartAt;
+            EndAt = model.EndAt;
+            Locale = model.Locale;
+            Enrollments = model.Enrollments.SelectNotNull(m => new Enrollment(api, m));
+            TotalStudents = model.TotalStudents;
+            Calendar = model.Calendar;
+            DefaultView = model.DefaultView;
+            SyllabusBody = model.SyllabusBody;
+            NeedsGradingCount = model.NeedsGradingCount;
+            Term = model.Term.ConvertIfNotNull(m => new Term(api, m));
+            CourseProgress = model.CourseProgress.ConvertIfNotNull(m => new CourseProgress(api, m));
+            ApplyAssignmentGroupWeights = model.ApplyAssignmentGroupWeights;
+            Permissions = model.Permissions;
+            IsPublic = model.IsPublic;
+            IsPublicToAuthUsers = model.IsPublicToAuthUsers;
+            PublicSyllabus = model.PublicSyllabus;
+            PublicSyllabusToAuth = model.PublicSyllabusToAuth;
+            PublicDescription = model.PublicDescription;
+            StorageQuotaMb = model.StorageQuotaMb;
+            StorageQuotaUsedMb = model.StorageQuotaUsedMb;
+            HideFinalGrades = model.HideFinalGrades;
+            License = model.License;
+            AllowStudentAssignmentEdits = model.AllowStudentAssignmentEdits;
+            AllowWikiComments = model.AllowWikiComments;
+            AllowStudentForumAttachments = model.AllowStudentForumAttachments;
+            OpenEnrollment = model.OpenEnrollment;
+            SelfEnrollment = model.SelfEnrollment;
+            RestrictEnrollmentsToCourseDates = model.RestrictEnrollmentsToCourseDates;
+            CourseFormat = model.CourseFormat;
+            AccessRestrictedByDate = model.AccessRestrictedByDate;
+            TimeZone = model.TimeZone;
+            Blueprint = model.Blueprint;
+            BlueprintRestrictions = model.BlueprintRestrictions;
+            BlueprintRestrictionsByObjectType = model.BlueprintRestrictionsByObjectType;
+        }
+
+        public string ToPrettyString() {
+            return "Course {" +
+                   ($"\n{nameof(Id)}: {Id}," +
+                   $"\n{nameof(SisCourseId)}: {SisCourseId}," +
+                   $"\n{nameof(Uuid)}: {Uuid}," +
+                   $"\n{nameof(IntegrationId)}: {IntegrationId}," +
+                   $"\n{nameof(SisImportId)}: {SisImportId}," +
+                   $"\n{nameof(Name)}: {Name}," +
+                   $"\n{nameof(CourseCode)}: {CourseCode}," +
+                   $"\n{nameof(WorkflowState)}: {WorkflowState}," +
+                   $"\n{nameof(AccountId)}: {AccountId}," +
+                   $"\n{nameof(RootAccountId)}: {RootAccountId}," +
+                   $"\n{nameof(EnrollmentTermId)}: {EnrollmentTermId}," +
+                   $"\n{nameof(GradingStandardId)}: {GradingStandardId}," +
+                   $"\n{nameof(CreatedAt)}: {CreatedAt}," +
+                   $"\n{nameof(StartAt)}: {StartAt}," +
+                   $"\n{nameof(EndAt)}: {EndAt}," +
+                   $"\n{nameof(Locale)}: {Locale}," +
+                   $"\n{nameof(Enrollments)}: {Enrollments}," +
+                   $"\n{nameof(TotalStudents)}: {TotalStudents}," +
+                   $"\n{nameof(Calendar)}: {Calendar}," +
+                   $"\n{nameof(DefaultView)}: {DefaultView}," +
+                   $"\n{nameof(SyllabusBody)}: {SyllabusBody}," +
+                   $"\n{nameof(NeedsGradingCount)}: {NeedsGradingCount}," +
+                   $"\n{nameof(Term)}: {Term}," +
+                   $"\n{nameof(CourseProgress)}: {CourseProgress}," +
+                   $"\n{nameof(ApplyAssignmentGroupWeights)}: {ApplyAssignmentGroupWeights}," +
+                   $"\n{nameof(Permissions)}: {Permissions}," +
+                   $"\n{nameof(IsPublic)}: {IsPublic}," +
+                   $"\n{nameof(IsPublicToAuthUsers)}: {IsPublicToAuthUsers}," +
+                   $"\n{nameof(PublicSyllabus)}: {PublicSyllabus}," +
+                   $"\n{nameof(PublicSyllabusToAuth)}: {PublicSyllabusToAuth}," +
+                   $"\n{nameof(PublicDescription)}: {PublicDescription}," +
+                   $"\n{nameof(StorageQuotaMb)}: {StorageQuotaMb}," +
+                   $"\n{nameof(StorageQuotaUsedMb)}: {StorageQuotaUsedMb}," +
+                   $"\n{nameof(HideFinalGrades)}: {HideFinalGrades}," +
+                   $"\n{nameof(License)}: {License}," +
+                   $"\n{nameof(AllowStudentAssignmentEdits)}: {AllowStudentAssignmentEdits}," +
+                   $"\n{nameof(AllowWikiComments)}: {AllowWikiComments}," +
+                   $"\n{nameof(AllowStudentForumAttachments)}: {AllowStudentForumAttachments}," +
+                   $"\n{nameof(OpenEnrollment)}: {OpenEnrollment}," +
+                   $"\n{nameof(SelfEnrollment)}: {SelfEnrollment}," +
+                   $"\n{nameof(RestrictEnrollmentsToCourseDates)}: {RestrictEnrollmentsToCourseDates}," +
+                   $"\n{nameof(CourseFormat)}: {CourseFormat}," +
+                   $"\n{nameof(AccessRestrictedByDate)}: {AccessRestrictedByDate}," +
+                   $"\n{nameof(TimeZone)}: {TimeZone}," +
+                   $"\n{nameof(Blueprint)}: {Blueprint}," +
+                   $"\n{nameof(BlueprintRestrictions)}: {BlueprintRestrictions}," +
+                   $"\n{nameof(BlueprintRestrictionsByObjectType)}: {BlueprintRestrictionsByObjectType}").Indent(4) + 
+                   "\n}";
+        }
+    }
+}
