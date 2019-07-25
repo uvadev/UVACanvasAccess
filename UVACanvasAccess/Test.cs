f45df193523d6cc19f16d4a65dce9ee3a15289e7
Max <mmorris@uview.academy>
2019-07-25 13:34:24 -0500

    Wrote csv map based file upload application.

--- UVACanvasAccess/Test.cs
+++ UVACanvasAccess/Test.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using dotenv.net;
 using UVACanvasAccess.ApiParts;
 using UVACanvasAccess.Debugging;
-using UVACanvasAccess.Util;
 
 namespace UVACanvasAccess {
     internal static class Test {
@@ -28,10 +28,39 @@ namespace UVACanvasAccess {
             Debug.Listeners.Add(new TraceToStdErr());
             DotEnv.Config();
             
-            var api = new Api(Environment.GetEnvironmentVariable("TEST_TOKEN"), 
+            var fileMapDir = Environment.GetEnvironmentVariable("TEST_MAP_DIR") 
+                             ?? throw new ArgumentException(".env should contain TEST_MAP_DIR");
+            if (!fileMapDir.EndsWith(Path.DirectorySeparatorChar.ToString())) {
+                fileMapDir += Path.PathSeparator;
+            }
+            
+            var api = new Api(Environment.GetEnvironmentVariable("TEST_TOKEN") 
+                              ?? throw new ArgumentException(".env should contain TEST_TOKEN"), 
                               "https://uview.instructure.com/api/v1/");
 
-            
+            foreach (var line in File.ReadLines(fileMapDir + "map.csv")) {
+                try {
+                    string[] halves = line.Split(',');
+                    Debug.Assert(halves.Length == 2);
+                    
+                    var (userKey, userFile) = (halves[0], halves[1]);
+
+                    var user = await api.StreamUsers(userKey)
+                                        .FirstAsync();
+                    
+                    var bytes = File.ReadAllBytes(fileMapDir + userFile);
+
+                    Console.WriteLine($"Preparing to upload filename {userFile} to user {userKey}, Id {user.Id}, SIS {user.SisUserId}");
+                    
+                    api.MasqueradeAs(user.Id);
+
+                    var file = await api.UploadPersonalFile(bytes, userFile, "test_csv_file_upload");
+
+                    Console.WriteLine($"Uploaded as {file.Id}!");
+                } finally {
+                    api.StopMasquerading();
+                }
+            }
         }
     }
 }
--- UVACanvasAccess/Util/IPrettyPrint.cs
+++ UVACanvasAccess/Util/IPrettyPrint.cs
@@ -1,14 +1,15 @@
+// ReSharper disable InvalidXmlDocComment
 namespace UVACanvasAccess.Util {
     
     /// <summary>
     /// Classes implementing this interface provide an implementation for formatted output in the form of:
     /// <code>
-    /// ClassName1 {
-    ///     Field1: foo,
-    ///     Field2: bar,
-    ///     Field3: ClassName2 {
-    ///         Field1: baz
-    ///     }
+    /// ClassName1 {<br/>
+    /// &nbsp;&nbsp;&nbsp;&nbsp;Field1: foo,<br/>
+    /// &nbsp;&nbsp;&nbsp;&nbsp;Field2: bar,<br/>
+    /// &nbsp;&nbsp;&nbsp;&nbsp;Field3: ClassName2 {<br/>
+    /// &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Field1: baz<br/>
+    /// &nbsp;&nbsp;&nbsp;&nbsp;}<br/>
     /// }
     /// </code>
     /// </summary>
