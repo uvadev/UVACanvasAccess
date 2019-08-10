814cdd7441eb47445a31683044593bf6f88ad520
Max <mmorris@uview.academy>
2019-08-10 10:30:21 -0500

    Added docs.

--- UVACanvasAccess/Builders/AppointmentGroupBuilder.cs
+++ UVACanvasAccess/Builders/AppointmentGroupBuilder.cs
@@ -11,6 +11,10 @@ using UVACanvasAccess.Util;
 
 namespace UVACanvasAccess.Builders {
 
+    /// <summary>
+    /// A class used to create or edit appointment groups using the builder pattern.
+    /// When all desired fields are set, call <see cref="Post"/> to execute the operation.
+    /// </summary>
     [PublicAPI]
     public class AppointmentGroupBuilder {
         private readonly Api _api;
@@ -44,6 +48,10 @@ namespace UVACanvasAccess.Builders {
             }
         }
 
+        /// <summary>
+        /// The subcontexts this group should be linked to.
+        /// </summary>
+        /// <returns>This builder.</returns>
         public AppointmentGroupBuilder WithSubContextCodes(IEnumerable<EventContext> subContexts) {
             foreach (var context in subContexts) {
                 PutArr("sub_context_codes", context.ContextCode);
@@ -52,34 +60,68 @@ namespace UVACanvasAccess.Builders {
             return this;
         }
 
+        /// <summary>
+        /// The description of this group.
+        /// </summary>
+        /// <returns>This builder.</returns>
         public AppointmentGroupBuilder WithDescription(string description) {
             return Put("description", description);
         }
 
+        /// <summary>
+        /// The name of the location of this group.
+        /// </summary>
+        /// <returns>This builder.</returns>
         public AppointmentGroupBuilder WithLocationName(string location) {
             return Put("location_name", location);
         }
 
+        /// <summary>
+        /// The address of the location of this group.
+        /// </summary>
+        /// <returns>This builder.</returns>
         public AppointmentGroupBuilder WithLocationAddress(string address) {
             return Put("location_address", address);
         }
 
-        public AppointmentGroupBuilder Published(bool publish = true) {
-            return Put("publish", publish.ToShortString());
+        /// <summary>
+        /// Irreversibly publish this group.
+        /// </summary>
+        /// <returns>This builder.</returns>
+        public AppointmentGroupBuilder Published() {
+            return Put("publish", "1");
         }
 
+        /// <summary>
+        /// The maximum amount of participants per appointment.
+        /// </summary>
+        /// <returns>This builder.</returns>
         public AppointmentGroupBuilder WithMaxParticipants(uint participants) {
             return Put("participants_per_appointment", participants.ToString());
         }
 
+        /// <summary>
+        /// The maximum amount of appointments any participant may schedule.
+        /// </summary>
+        /// <returns>This builder.</returns>
         public AppointmentGroupBuilder WithMaxAppointments(uint appointments) {
             return Put("max_appointments_per_participant", appointments.ToString());
         }
         
+        /// <summary>
+        /// The minimum amount of appointments any participant must schedule.
+        /// </summary>
+        /// <returns>This builder.</returns>
         public AppointmentGroupBuilder WithMinAppointments(uint appointments) {
             return Put("min_appointments_per_participant", appointments.ToString());
         }
 
+        /// <summary>
+        /// Add one time slot to the appointment group that can be scheduled by participants.
+        /// </summary>
+        /// <param name="start">The beginning of the time slot.</param>
+        /// <param name="end">The end of the time slot.</param>
+        /// <returns>This builder.</returns>
         public AppointmentGroupBuilder AddTimeSlot(DateTime start, DateTime end) {
             PutArr2("new_appointments", _dateN.ToString(), start.ToIso8601Date());
             PutArr2("new_appointments", _dateN.ToString(), end.ToIso8601Date());
@@ -87,10 +129,18 @@ namespace UVACanvasAccess.Builders {
             return this;
         }
 
+        /// <summary>
+        /// Whether the participants in this group should have protected or private visibility.
+        /// </summary>
+        /// <returns>This builder.</returns>
         public AppointmentGroupBuilder WithProtectedVisibility(bool @protected = true) {
             return Put("participant_visibility", @protected ? "protected" : "private");
         }
 
+        /// <summary>
+        /// Performs the operation.
+        /// </summary>
+        /// <returns>The newly created or edited appointment group.</returns>
         public Task<AppointmentGroup> Post() {
             if (!_isEditing) {
                 return _api.PostCreateAppointmentGroup(this);
