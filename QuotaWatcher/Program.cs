d09bf9123cc9697c930b4dc3695d1de2502f3dcc
Max <mmorris@uview.academy>
2019-09-17 09:48:03 -0500

    Finished Quota Watcher app.

--- QuotaWatcher/Program.cs
+++ QuotaWatcher/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using dotenv.net;
 using UVACanvasAccess.ApiParts;
+using UVACanvasAccess.Structures;
 using UVACanvasAccess.Util;
 
 namespace QuotaWatcher {
@@ -12,7 +13,7 @@ namespace QuotaWatcher {
             "We noticed that your personal file storage folder is {1}% full ({2} mb used).\n" +
             "This might cause problems in the future if important files ever need to be automatically uploaded to your account.\n\n" +
             "Please make some more room in your personal folder by deleting some files you don't need. You can do that here: https://uview.instructure.com/files. Make sure you check every subfolder under \"My Files\".\n\n" +
-            "If you don't do this, some of your files may get deleted automatically if something ever needs to be uploaded to your account. If that happens, you won't be able to recover those files.\n\n" +
+            "If you don't do this, some of your files may get deleted automatically in the future. If that happens, you won't be able to recover those files.\n\n" +
             "Thanks.\n\n" +
             "[This message was generated automatically.]";
         
@@ -27,6 +28,7 @@ namespace QuotaWatcher {
                 return;
             }
 
+            uint nagged = 0;
             await foreach (var user in api.StreamUsers()) {
                 try {
                     api.MasqueradeAs(user.Id);
@@ -43,13 +45,22 @@ namespace QuotaWatcher {
                                                 user.Name,
                                                 Math.Round(used / quota * 100, 2), 
                                                 Math.Round(used, 3));
-                    
+
+                    await foreach (var c in api.CreateConversation(new QualifiedId[] {user.Id},
+                                                                   message,
+                                                                   "File Storage Alert",
+                                                                   true)) {
+                        Console.WriteLine($"sent the message to {user.Id}.\n{c.ToPrettyString()}\n------\n");
+                        ++nagged;
+                    }
                 } catch (Exception e) {
                     Console.WriteLine(e);
                 } finally {
                     api.StopMasquerading();
                 }
             }
+
+            Console.WriteLine($"nagged {nagged} users.");
         }
     }
 }
